b573acb14a6079980cb75a667623a91ac96050b7
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-17 03:48:10 +0700

    chore: update dependencies and drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,8 +12,8 @@ if (!process.env.DATABASE_URL) {
   process.exit(1);
 }
 export default {
-  schema: "./lib/db/schema",
-  out: "./lib/db/migrations",
+  schema: "./src/lib/db/schema",
+  out: "./src/lib/db/migrations",
   dialect: "postgresql",
   verbose: true,
   dbCredentials: { url: process.env.DATABASE_URL },
